233f26c157589113b118b4c60ca5ad350c44f201
tf-vishal <vvishalsharma0609@gmail.com>
2025-06-29 21:38:53 +0530

    models

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,1 +1,21 @@
 package models
+
+import (
+	"github.com/tf-vishal/Go-BookStore/pkg/config"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.model
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
