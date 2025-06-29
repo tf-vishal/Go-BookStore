7eb229fb743009fe781f4d9fd9b7324ff67269f8
tf-vishal <vvishalsharma0609@gmail.com>
2025-06-29 20:43:47 +0530

    DB Connection Config

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,1 +1,21 @@
 package config
+
+import (
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "ego:lallantap@/simplerest?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
