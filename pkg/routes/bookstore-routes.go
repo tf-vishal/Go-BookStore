710260f981a643e88727af2f1845b9cef922cbbb
tf-vishal <vvishalsharma0609@gmail.com>
2025-06-29 20:01:36 +0530

    Routes added

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -3,5 +3,12 @@ package routes
 import (
 	"github.com/gorilla/mux"
 	"github.com/tf-vishal/Go-BookStore/pkg/controllers"
-	
 )
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBooks).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
