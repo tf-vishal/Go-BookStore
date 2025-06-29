30f2fb3fcf911845961a2e28eabd0cd626f1a2b4
tf-vishal <vvishalsharma0609@gmail.com>
2025-06-29 21:05:54 +0530

    unmarshal the data

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,1 +1,17 @@
 package utils
+
+//The data will be in json format. So to use it by controllers, we need to unmarshal it.
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+	"net/http"
+)
+
+func ParseBody(r *http.Request,x interface{}){
+	if body, err := io.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal([]byte(body), x); err != nil {
+			return
+		}
+}
