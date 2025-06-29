31f68b46ca8f7931d90b023cd6badd7d1becab4c
tf-vishal <vvishalsharma0609@gmail.com>
2025-06-29 22:14:05 +0530

    main go file

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/tf-vishal/Go-BookStore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
